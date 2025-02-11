a980d804f1024711e5f376146604099f4b2b1296
Denismaka <makadenis370@gmail.com>
2025-02-11 11:38:18 +0100

    Mise en place du script de srollreveal

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -20,3 +20,14 @@ navigationItems.forEach((navigationItem) => {
     navigation.classList.remove('active');
   });
 });
+
+ScrollReveal({
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal('.container, .heading', { origin: 'top' });
+ScrollReveal().reveal('img, .col-md-4, form', { origin: 'bottom' });
+ScrollReveal().reveal('h1, h3, h4, h5, span, li', { origin: 'left' });
+ScrollReveal().reveal('p, i, .row', { origin: 'right' });
